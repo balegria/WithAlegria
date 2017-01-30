4c3af146fade51962d9b253f4bb256ebcef37650
Betsy Alegria <balegria@stanford.edu>
2017-01-30 14:16:52 -0800

    adjusted port

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ MongoClient.connect(mongoDBURL, (err, database) => {
 	else {
 		db = database;
 		//only want to start our servers when DB is connected
-		app.listen(3000, () => {
+		app.listen((process.env.PORT || 3000), () => {
 			console.log('listening on 3000');
 		})
 	}
