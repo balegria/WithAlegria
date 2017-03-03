d6d0344c54bfb3b4e539a6da4f217fd400e2e73c
Betsy Alegria <balegria@stanford.edu>
2017-03-02 22:55:53 -0800

    changed the layout

--- public/js/mainApp.js
+++ public/js/mainApp.js
@@ -8,5 +8,5 @@ $(document).on('click', '.navbar-button', function(event){
 	// turns all nav bar items one color
 	$(".navbar-button").css('color', '#0F161A');
 	// turns the selected nav bar item a lighter color
-	$(this).css('color', '#C2925E');
+	$(this).css('color', '#EFAA52');
 })
