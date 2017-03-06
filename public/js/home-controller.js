39720fc9f497c7ba580aedbde318388f31fbf574
Betsy Alegria <balegria@stanford.edu>
2017-03-05 19:36:49 -0800

    changed design/layout of homepage

--- public/js/home-controller.js
+++ public/js/home-controller.js
@@ -11,9 +11,9 @@ app.controller('PageCtrl', ['$scope', '$http', '$resource',
             get: 'user',
             userId: '176659272',
             accessToken: '4483004649.ba4c844.be8c9996027d45b1ab46b078fd6abb9d',
-            // only show the last three images
             limit: 3,
-            resolution: 'low_resolution'
+            resolution: 'low_resolution',
+            template: '<div class="insta-img"><a href="{{link}}"><img src="{{image}}" /></a></div>'
       });
       userFeed.run();
     }
